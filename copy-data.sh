9048a57d12ae28207df978e0ba749bfba7f85e6c
Jan Warchoł <jan.warchol@gmail.com>
2015-08-19 15:02:50 +0200

    Add old script for copying data from backup

---
+++ copy-data.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+set -o nounset
+set -o errexit
+
+# Copy data from backup.
+# First argument should point to directory with my data.
+
+RED="\e[31m"; GREEN="\e[32m"; RESET="\e[0m"
+
+copy_data() {
+    SRC="$1"
+    DEST="$2"
+
+    echo -e "\nTransferring personal data from $SRC to $DEST..."
+
+    PATHS_TO_COPY=(
+        "janek"
+        "media"
+        "repos"
+        "zasoby"
+        ".fonts"
+        ".mozilla"
+        ".ssh"
+        ".bash_history"
+        ".config/dotfiles-git-dir"
+        ".config/terminator"
+        ".config/monitors.xml"
+        ".config/system-setup-todo"
+        #".kde/share/apps/kate"
+        #".kde/share/config/katerc"
+        ".vagrant.d"
+        ".vim"
+        ".vimrc"
+        #.chrome
+        #virtualbox vms
+    )
+
+    SRC_PARTITION=$(df -P "$SRC" | tail -1 | cut -d' ' -f1)
+    DEST_PARTITION=$(df -P "$DEST" | tail -1 | cut -d' ' -f1)
+
+    if [ $SRC_PARTITION = $DEST_PARTITION ]; then
+        command="mv"
+        verb="Moving"
+    else
+        command="cp --recursive"
+        verb="Copying"
+    fi
+
+    for path in ${PATHS_TO_COPY[*]}; do
+        if [ -e "$SRC"/"$path" ]; then
+            if [ -e "$DEST"/"$path" ]; then
+                echo -e ${RED}"$DEST"/"$path" already exists, renaming...${RESET}
+                echo "$path" >> ~/conflicting-paths.txt
+                mv "./$path" "./$path.old" --backup=numbered
+            fi
+            echo -n $verb "$path"...
+            mkdir --parents "$DEST"/$(dirname "$path")/
+            $command "$SRC"/"$path" "$DEST"/$(dirname "$path")/
+            echo -e ${GREEN} done.${RESET}
+        fi
+        sleep 0.1
+    done
+
+    mkdir --parents "$DEST"/media
+    echo -e "${GREEN}Data transferred.\n${RESET}"
+    sleep 3
+}
+
+copy_data "$1" "$HOME"
