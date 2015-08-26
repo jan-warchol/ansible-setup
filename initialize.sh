ccb82f1d713715832817fb4ea8f4b407e840f56b
Jan Warchoł <jan.warchol@gmail.com>
2015-08-26 08:50:38 +0200

    Host the repository on Bitbucket

--- initialize.sh
+++ initialize.sh
@@ -4,4 +4,4 @@ sudo apt-add-repository --yes ppa:ansible/ansible
 sudo apt-get --yes update
 sudo apt-get --yes install ansible git
 
-git clone https://github.com/janek-warchol/ansible-system-setup.git repos/ansible-system-setup
+git clone https://janek-warchol@bitbucket.org/janek-warchol/ansible-system-setup.git repos/ansible-system-setup
