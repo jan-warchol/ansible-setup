506c6c36361e143ce966986b75d0558e13d09025
Jan Warchoł <jan.warchol@gmail.com>
2016-02-22 16:07:53 +0100

    Fix missing national character

--- roles/install-ansible-setup/files/passwordstore.py
+++ roles/install-ansible-setup/files/passwordstore.py
@@ -1,4 +1,4 @@
-# (c) 2012, Jan Warchol <jan.warchol(at)gmail.com>
+# (c) 2012, Jan Warchoł <jan.warchol(at)gmail.com>
 #
 # Ansible is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
